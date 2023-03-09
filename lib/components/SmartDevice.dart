47735707b66318f24f34ffa7193c95b67ac79b6a
SamsonMokaya <mokayasamson@gmail.com>
2023-03-09 11:35:06 +0300

    Smooth UI

---
+++ lib/components/SmartDevice.dart
@@ -0,0 +1,62 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SmartDevice extends StatelessWidget {
+  final String device;
+  final String iconPath;
+  final bool powerOn;
+  final Function(bool)? onChanged;
+
+  const SmartDevice({
+    super.key,
+    required this.device,
+    required this.iconPath,
+    required this.powerOn,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: powerOn? Colors.grey[900] : Colors.grey[200], borderRadius: BorderRadius.circular(15)),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Image.asset(
+              iconPath,
+              height: 60,
+              color: powerOn? Colors.grey[200] : Colors.grey[800],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    device,
+                    style: TextStyle(
+                      color:powerOn? Colors.grey[200] : Colors.grey[800],
+                      fontFamily: 'Material Icons',
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Transform.rotate(
+                  angle: pi / 2,
+                  child: CupertinoSwitch(
+                    value: powerOn,
+                    onChanged: onChanged,
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smarthome/components/SmartDevice.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,16 +9,93 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late double padd = 25.0;
+
+  List mySMartDevices = [
+    ['Smart TV', 'lib/assets/smart-tv.png', false],
+    ['Smart AC', 'lib/assets/air-conditioner.png', true],
+    ['Smart FUN', 'lib/assets/fan.png', true],
+    ['Smart BULB', 'lib/assets/light-bulb.png', true],
+    ['Smart BULB', 'lib/assets/light-bulb.png', true],
+    ['Smart BULB', 'lib/assets/light-bulb.png', true],
+  ];
+
+  void powerChanged(int index,bool value) {
+   setState(() {
+     mySMartDevices[index][2] = value;
+   });
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.grey[300],
       body: SafeArea(
-        child: Column(
-          children:[
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        // App bar icons
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: padd, vertical: padd),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            //menu icon
+            Image.asset(
+              'lib/assets/menu.png',
+              height: 45,
+              color: Colors.grey[800],
+            ),
 
-          ]
-        )),
+            Icon(
+              Icons.person,
+              color: Colors.grey[800],
+            )
+          ]),
+        ),
+
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: padd),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Text("Hello there",
+                style: TextStyle(
+                  color: Colors.grey[800],
+                )),
+            Text(
+              "Samson Mokaya",
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.grey[850],
+              ),
+            ),
+            SizedBox(
+              height: padd,
+            ),
+            Text("Smart Devices",
+                style: TextStyle(
+                  color: Colors.grey[800],
+                ))
+          ]),
+        ),
+
+        Expanded(
+            child: GridView.builder(
+                itemCount: mySMartDevices.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.all(padd),
+                    child: SmartDevice(
+                      device: mySMartDevices[index][0],
+                      iconPath: mySMartDevices[index][1],
+                      powerOn: mySMartDevices[index][2],
+                      onChanged: (value) => powerChanged(index, value),
+                    ),
+                  );
+                }))
+      ])),
     );
   }
 }
+
+
